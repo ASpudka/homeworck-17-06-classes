50b6e00d7537cb0742edf6b4d2cf8ace6de4f6fe
Aleksandra <aspudka@gmail.com>
2022-06-22 19:24:47 +0300

    Add children class, but have mistake is getVolume

--- figure.js
+++ figure.js
@@ -4,7 +4,58 @@ class Figura3D {
   constructor(name) {
     this.name = name;
   }
+  getPerimetr() {
+    return null;
+  }
+
   getVolume() {
     return null;
   }
 }
+
+class Piramide extends Figura3D {
+  //наследование родительского класса
+  constructor(sideFoot1, sideFoot2, sideFoot3, hight) {
+    super("Piramide");
+    this.sideFoot1 = sideFoot1;
+    this.sideFoot2 = sideFoot2;
+    this.sideFoot3 = sideFoot3;
+    this.hight = hight;
+  }
+  getPerimetr() {
+    return this.sideFoot1 + this.sideFoot2 + this.sideFoot3;
+  }
+
+  getVolume() {
+    return (this.getPerimetr * this.hight) / 3;
+  }
+}
+const per = new Piramide(3, 4, 5, 2);
+ 
+class Cub extends Figura3D{
+  constructor(side){
+    super ("Cub");
+    this.side = side;
+  }
+  getVolume() {
+    return this.side*this.side*this.side;
+  }
+};
+const cub = new Cub(2);
+
+class Cylinder extends Figura3D{
+  constructor(radius, hight){
+    super ("Cylinder");
+    this.radius = radius;
+    this.hight = hight;
+  }
+  getPerimetr() {
+    return 2*Math.PI*this.radius;
+  }
+
+  getVolume() {
+    return this.getPerimetr * this.hight;
+  }
+}
+
+const cyl = new Cylinder(2,1);
